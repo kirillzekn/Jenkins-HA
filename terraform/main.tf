e6840f46bd6e98a5fc73049b55c1767c586713cd
Kirill <Kirill@Kirill.com>
2025-03-19 13:16:10 +0500

    fix rg name

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "jenkins-ha" {
   name                = "jenkins-ha-kv"
   location            = azurerm_resource_group.jenkins-ha.location
-  resource_group_name = azurerm_resource_group.jenkins-ha
+  resource_group_name = azurerm_resource_group.jenkins-ha.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
   purge_protection_enabled    = false
