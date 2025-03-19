fb0f97216367a38b6b916373c9f37f3ea444af90
Kirill <Kirill@Kirill.com>
2025-03-19 15:54:05 +0500

    vmss alb

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,65 +43,95 @@ resource "azurerm_key_vault_secret" "jenkins-ha-ssh" {
 }
 
 #LoadBalancerResource
-module "vmscaleset" {
-  source = "Azure/vm-scale-sets/azurerm"
-
-  # Resource Group and location, VNet and Subnet detials (Required)
-  resource_group_name  = azurerm_resource_group.jenkins-ha.name
+resource "azurerm_public_ip" "jenkins-ha" {
+  name                = "PublicIPForLB"
   location            = azurerm_resource_group.jenkins-ha.location
-  virtual_network_name = "vnet-default-hub-westeurope"
-  subnet_name          = "snet-management-default-hub-westeurope"
-  vmscaleset_name      = "jenkins-ha-vmss"
-  virtual_machine_size = "Standard_B2ls_v2"
+  resource_group_name = azurerm_resource_group.jenkins-ha.name
+  allocation_method   = "Static"
+}
 
+resource "azurerm_lb" "jenkins-ha" {
+  name                = "TestLoadBalancer"
+  location            = azurerm_resource_group.jenkins-ha.location
+  resource_group_name = azurerm_resource_group.jenkins-ha.name
 
-  # This module support multiple Pre-Defined Linux and Windows Distributions.
-  # These distributions support the Automatic OS image upgrades in virtual machine scale sets
-  # Linux images: ubuntu1804, ubuntu1604, centos75, coreos
-  # Windows Images: windows2012r2dc, windows2016dc, windows2019dc, windows2016dccore
-  # Specify the RSA key for production workloads and set generate_admin_ssh_key argument to false
-  # When you use Autoscaling feature, instances_count will become default and minimum instance count. 
-  os_flavor               = "linux"
-  linux_distribution_name = "ubuntu1804"
-  generate_admin_ssh_key  = false
-  admin_ssh_key_data      = tls_private_key.jenkins-ha.public_key_openssh
-  instances_count         = 2
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.jenkins-ha.id
+  }
+}
 
-  # Public and private load balancer support for VM scale sets
-  # Specify health probe port to allow LB to detect the backend endpoint status
-  # Standard Load Balancer helps load-balance TCP and UDP flows on all ports simultaneously
-  # Specify the list of ports based on your requirement for Load balanced ports
-  # for additional data disks, provide the list for required size for the disk. 
-  load_balancer_type              = "public"
-  load_balancer_health_probe_port = 80
-  load_balanced_port_list         = [80, 443]
-  additional_data_disks           = [100, 200]
+#Deploy VMSS
+resource "azurerm_virtual_network" "jenkins-ha" {
+  name                = "jenkins-ha-vnet"
+  resource_group_name = azurerm_resource_group.jenkins-ha.name
+  location            = azurerm_resource_group.jenkins-ha.location
+  address_space       = ["10.0.0.0/16"]
+}
 
-  # Enable Auto scaling feature for VM scaleset by set argument to true. 
-  # Instances_count in VMSS will become default and minimum instance count.
-  # Automatically scale out the number of VM instances based on CPU Average only.    
-  enable_autoscale_for_vmss          = true
-  minimum_instances_count            = 1
-  maximum_instances_count            = 2
-  scale_out_cpu_percentage_threshold = 80
-  scale_in_cpu_percentage_threshold  = 20
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.jenkins-ha.name
+  virtual_network_name = azurerm_resource_group.jenkins-ha.location
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-  # Network Seurity group port allow definitions for each Virtual Machine
-  # NSG association to be added automatically for all network interfaces.
-  # SSH port 22 and 3389 is exposed to the Internet recommended for only testing. 
-  # For production environments, we recommend using a VPN or private connection
-  nsg_inbound_rules = [
-    {
-      name                   = "http"
-      destination_port_range = "80"
-      source_address_prefix  = "*"
-    },
+resource "azurerm_linux_virtual_machine_scale_set" "jenkins-ha" {
+  name                = "jenkins-ha-vmss"
+  resource_group_name = azurerm_resource_group.jenkins-ha.name
+  location            = azurerm_resource_group.jenkins-ha.location
 
-    {
-      name                   = "https"
-      destination_port_range = "443"
-      source_address_prefix  = "*"
-    },
-  ]
+  sku                 = "Standard_B2s"
+  instances           = 2
+  admin_username      = "adminuser"
 
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.jenkins-ha.public_key_openssh
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "jenkins-ha-nic"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.internal.id
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "jenkins-ha-nsg" {
+  name                = "jenkins-ha-nsg"
+  location            = azurerm_resource_group.jenkins-ha.location
+  resource_group_name = azurerm_resource_group.jenkins-ha.name
+  security_rule = {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }  
+}
+
+resource "azurerm_subnet_network_security_group_association" "jenkins-ha" {
+  subnet_id                 = azurerm_subnet.internal.id
+  network_security_group_id = azurerm_network_security_group.jenkins-ha-nsg.id
 }
