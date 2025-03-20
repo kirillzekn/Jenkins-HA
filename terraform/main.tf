8ddb559bd772b172141fb22bc6f459412f828fb2
Kirill <Kirill@Kirill.com>
2025-03-20 09:56:43 +0500

    fixed subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,7 +72,7 @@ resource "azurerm_virtual_network" "jenkins-ha" {
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.jenkins-ha.name
-  virtual_network_name = azurerm_resource_group.jenkins-ha.location
+  virtual_network_name = azurerm_virtual_network.jenkins-ha.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
