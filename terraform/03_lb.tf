36e69d30ce33eaf181c3ae8ceb19fbb4ef0f3777
Kirill <Kirill@Kirill.com>
2025-04-08 14:14:24 +0500

    LB frontend port

--- terraform/03_lb.tf
+++ terraform/03_lb.tf
@@ -26,8 +26,9 @@ resource "azurerm_lb_backend_address_pool" "jenkins-ha" {
 resource "azurerm_lb_nat_rule" "jenkins-ha-22" {
   name                           = "SSH_22"
   protocol                       = "Tcp"
-  frontend_port_start            = 22
-  frontend_port_end              = 24
+  # frontend_port_start            = 22
+  # frontend_port_end              = 24
+  frontend_port                  = 22
   backend_port                   = 22
   frontend_ip_configuration_name = "PublicIPAddress"
   loadbalancer_id                = azurerm_lb.jenkins-ha.id
