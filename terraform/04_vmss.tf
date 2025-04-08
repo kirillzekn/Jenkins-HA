7c85ea8670327102062a5773f0b85d067aa14aae
Kirill <Kirill@Kirill.com>
2025-04-08 11:28:15 +0500

    fmt

--- terraform/04_vmss.tf
+++ terraform/04_vmss.tf
@@ -94,11 +94,11 @@ resource "azurerm_subnet_network_security_group_association" "jenkins-ha" {
 
 #Azure VMSS Extension vmss script
 resource "azurerm_virtual_machine_scale_set_extension" "jenkins-ha" {
-  name                 = "jenkins-ha-vmss-script"
+  name                         = "jenkins-ha-vmss-script"
   virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.jenkins-ha.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
+  publisher                    = "Microsoft.Azure.Extensions"
+  type                         = "CustomScript"
+  type_handler_version         = "2.0"
 
   protected_settings = <<PROTECTED_SETTINGS
     {
